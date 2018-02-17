f245ac9da98a7f286e92f13d72e4f4ea1d11d173
Nick Johnson <nick.johnson@m.kckps.org>
2018-02-16 19:08:07 -0600

    updated code for robot control

--- src/org/usfirst/frc/team5189/robot/Robot.java
+++ src/org/usfirst/frc/team5189/robot/Robot.java
@@ -1,17 +1,13 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package org.usfirst.frc.team5189.robot;
 
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.VictorSP;
+
+import org.usfirst.frc.team5189.robot.DriveBase;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,10 +17,13 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  * directory.
  */
 public class Robot extends IterativeRobot {
-	private DifferentialDrive m_robotDrive
-			= new DifferentialDrive(new Spark(0), new Spark(1));
-	private Joystick m_stick = new Joystick(0);
-	private Timer m_timer = new Timer();
+	
+	private DriveBase m_driveBase
+		=new DriveBase(new VictorSP(0), new VictorSP(1), new VictorSP(2), new VictorSP(3));
+	private XboxController m_controller= new XboxController(0);
+	private Joystick m_joystick= new Joystick(0);
+	private Hand hand;
+	
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -32,72 +31,55 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void robotInit() {
+		
+		
+		
+		
+		
 	}
 
-	
-	
-	
-	
-	
+
 	/**
 	 * This function is run once each time the robot enters autonomous mode.
 	 */
 	@Override
 	public void autonomousInit() {
-		m_timer.reset();
-		m_timer.start();
+	
 	}
 
-	
-	
-	
-	
-	
 	/**
 	 * This function is called periodically during autonomous.
 	 */
 	@Override
 	public void autonomousPeriodic() {
-		// Drive for 2 seconds
-		if (m_timer.get() < 2.0) {
-			m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
-		} else {
-			m_robotDrive.stopMotor(); // stop robot
-		}
+		
 	}
 
-	
-	
-	
-	
-	
 	/**
 	 * This function is called once each time the robot enters teleoperated mode.
 	 */
 	@Override
 	public void teleopInit() {
+		
+	
 	}
 
-	
-	
-	
-	
-	
-	
+
+
+
 	/**
 	 * This function is called periodically during teleoperated mode.
 	 */
 	@Override
 	public void teleopPeriodic() {
-		m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+		
+		
+		m_driveBase.driveCartesian(m_joystick.getRawAxis(0), m_controller.getY()*-1, m_controller.getX()*-1);
+		
+		
 	}
 
-	
-	
-	
-	
-	
-	
+
 	/**
 	 * This function is called periodically during test mode.
 	 */
