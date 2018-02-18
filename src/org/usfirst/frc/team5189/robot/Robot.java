a1f4070322395a4043c61fe958ea7139874dbd21
Bob Campbell <mostlybob@gmail.com>
2018-02-18 07:24:37 -0600

    added reverse of existing test

--- src/org/usfirst/frc/team5189/robot/Robot.java
+++ src/org/usfirst/frc/team5189/robot/Robot.java
@@ -20,58 +20,39 @@ public class Robot extends IterativeRobot {
 	private XboxController m_controller= new XboxController(0);
 	private Joystick m_joystick= new Joystick(0);
 	
-
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
 	@Override
 	public void robotInit() {
 	}
 
-
-	/**
-	 * This function is run once each time the robot enters autonomous mode.
-	 */
 	@Override
 	public void autonomousInit() {
 	}
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
 	@Override
 	public void autonomousPeriodic() {
 	}
 
-	/**
-	 * This function is called once each time the robot enters teleoperated mode.
-	 */
 	@Override
 	public void teleopInit() {
 	}
 
-	/**
-	 * This function is called periodically during teleoperated mode.
-	 */
 	@Override
 	public void teleopPeriodic() {
 		m_driveBase.driveCartesian(m_joystick.getRawAxis(0), m_controller.getY()*-1, m_controller.getX()*-1);
 	}
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
 	@Override
 	public void testPeriodic() {
 	}
 
 	@Override
 	public  void testInit() {
-		//System.out.println(m_joystick.getRawAxis(1));
-		//System.out.println(m_controller.getY());
-		//System.out.println(m_controller.getX());
 		m_driveBase.driveCartesian(.5, 0, 0);
 		Timer.delay(5.0);
+
+		m_driveBase.driveCartesian(-.5, 0, 0);
+		Timer.delay(5.0);
+		
+		
 	}
 }
